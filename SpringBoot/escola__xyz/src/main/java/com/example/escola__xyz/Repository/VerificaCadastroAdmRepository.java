7a60998cf4c5bd399e048b5d49b914010ece67c0
Hope <ana.s.morais@aluno.senai.br>
2026-03-26 08:33:08 -0300

    Delete SpringBoot directory

--- SpringBoot/escola__xyz/src/main/java/com/example/escola__xyz/Repository/VerificaCadastroAdmRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.escola__xyz.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.escola__xyz.Model.VerificaCadastroAdm;;
-
-public interface VerificaCadastroAdmRepository extends CrudRepository <VerificaCadastroAdm, String>{
-    
-    VerificaCadastroAdm findByCpf(String cpf);
-}
